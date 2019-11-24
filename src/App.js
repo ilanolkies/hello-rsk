f6d1a155ceaf93e9b39d4a9638d4688d28f9ab48
Ilan Olkies <ilanolkies@outlook.com>
2019-11-24 02:53:34 -0300

    Set greeting to testnet

--- src/App.js
+++ src/App.js
@@ -15,10 +15,14 @@ class App extends Component {
 
     this.state = {
       getting: false,
-      greeting: null
+      greeting: null,
+      value: '',
+      setting: false,
     };
 
     this.getGreeting = this.getGreeting.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.setGreeting = this.setGreeting.bind(this);
   }
 
   componentDidMount () {
@@ -36,8 +40,35 @@ class App extends Component {
     });
   }
 
+  handleChange (event) {
+    const { value } = event.target;
+    this.setState({ value });
+  }
+
+  setGreeting (event) {
+    event.preventDefault();
+    this.setState({ setting: true });
+
+    const { value } = this.state;
+
+    const web3 = new Web3(window.web3);
+    const helloRSK = new web3.eth.Contract(helloRSKAbi, helloRSKAddress);
+
+    window.web3.currentProvider.enable()
+    .then(accounts => helloRSK.methods.setGreeting(value).send({ from: accounts[0] }))
+    .then(console.log)
+    .catch(console.error);
+
+    this.setState({ setting: false });
+  }
+
   render () {
-    const { greeting, getting } = this.state;
+    const {
+      greeting,
+      getting,
+      value,
+      setting
+    } = this.state;
 
     return (
       <div className="App">
@@ -54,11 +85,24 @@ class App extends Component {
           <div>
             <p>Greeting: <b>{getting ? '...' : greeting}</b></p>
           </div>
-          <form>
-            <input type="text" className="Greeting-input" />
-            <button type="submit" className="Greeting-submit">setGreeting()</button>
+          <form onSubmit={this.setGreeting}>
+            <input
+              type="text"
+              className="Greeting-input"
+              value={value}
+              onChange={this.handleChange}
+              disabled={setting}
+            />
+            {
+              !window.web3 ?
+              <a href="https://chrome.google.com/webstore/detail/nifty-wallet/jbdaocneiiinmjbjlgalhcelgbejmnid" target="_blank" rel="noopener noreferrer">Get Nifty!</a> :
+              <input type="submit" value="setGreeting()" className="Greeting-submit" disabled={!window.web3 || setting} />
+            }
           </form>
         </main>
+        <footer className="App-footer">
+          Find the smart contract <a href="https://explorer.testnet.rsk.co/address/0x0fb49bb37ba4b0186a87c866a2bbd29e1ef378da" target="_blank" rel="noopener noreferrer">here</a>
+        </footer>
       </div>
     );
   }
