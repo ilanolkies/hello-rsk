8ca88ff072bbdbdfa88efacebbe632bcfcf5bd65
Ilan Olkies <ilanolkies@outlook.com>
2019-11-24 03:35:50 -0300

    Updates

---
+++ precache-manifest.991983e634ff47a8d00fe1e22a944204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709014b046c1b8bf222884cff52fd81f",
+    "url": "/hello-rsk/index.html"
+  },
+  {
+    "revision": "f17043550b097333660b",
+    "url": "/hello-rsk/static/css/main.f6dd4075.chunk.css"
+  },
+  {
+    "revision": "cdeea60a3b5c9a79acc2",
+    "url": "/hello-rsk/static/js/2.5563eb27.chunk.js"
+  },
+  {
+    "revision": "f17043550b097333660b",
+    "url": "/hello-rsk/static/js/main.463d0ba9.chunk.js"
+  },
+  {
+    "revision": "db9321de2b1a7ea3a9cb",
+    "url": "/hello-rsk/static/js/runtime-main.aef7fe4d.js"
+  },
+  {
+    "revision": "f0fe53bb70355dad0a519ec0d5e180c0",
+    "url": "/hello-rsk/static/media/logo.f0fe53bb.svg"
+  }
+]);
